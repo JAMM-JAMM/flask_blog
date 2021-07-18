7bae3839cc7956c26bbe60c6e637ac6285dcfe02
ShimJaeMin <apvmf32@gmail.com>
2021-07-19 02:43:06 +0900

    feat: datetime format 변경 1. 템플릿 필터 만들기 2. 템플릿 필터 jinja filter 등록 3. 템플릿 필터 적용

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -5,10 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 import config
 
-# 중요!
-# db 객체를 create_app 함수 안에서 생성하면 블루프린트와 같은 다른 모듈에서 불러올 수 없다.
-# 따라서, db와 migrate 객체를 create_app 함수 밖에서 전역 변수로 선언하고
-# create_app 함수 안에서 'init_app' 메서드를 이용하여 객체를 초기화해야 함.
 db = SQLAlchemy()
 migrate = Migrate()
 
@@ -18,14 +14,13 @@ def create_app():
 
     app = Flask(__name__)
 
-    # config.py 파일에 작성한 항목을 app.config 환경 변수로 부르기 위해
-    # app.config.from_object(config) 코드 추가
     app.config.from_object(config)
 
     # ORM
     db.init_app(app)
     migrate.init_app(app, db)
 
+    # Blueprint
     from .views import main_views
     app.register_blueprint(main_views.bp)
 
@@ -34,5 +29,10 @@ def create_app():
 
     from .views import answer_views
     app.register_blueprint(answer_views.bp)
+
+    # Filter
+    from .filter import format_datetime
+    # datetime이라는 이름으로 필터 등록
+    app.jinja_env.filters['datetime'] = format_datetime
     
     return app
