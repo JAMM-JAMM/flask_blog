9339831233ad20982fb6e55e0df9a49ab01049a3
ShimJaeMin <apvmf32@gmail.com>
2021-07-24 20:05:11 +0900

    chore: config/production.py 수정

--- myproject/config/production.py
+++ myproject/config/production.py
@@ -2,5 +2,5 @@ from config.default import *
 
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
-SQLALCHEMY_TRACK_MODIFICATION = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'V\x1e\xb0Iw\x06J!i\x17\xaa\xce\x07\xb2\x97\r'
