af682dbc6ab84c5147b9d5bb975da7b6434e7569
ShimJaeMin <apvmf32@gmail.com>
2021-07-27 21:15:21 +0900

    feat: logging msg 설정

--- myproject/pybo/views/main_views.py
+++ myproject/pybo/views/main_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for
+from flask import Blueprint, url_for, current_app
 from werkzeug.utils import redirect
 
 from pybo.models import Question
@@ -13,7 +13,9 @@ def hello_pybo():
 # url_for: 라우트가 설정된 함수명으로 URL을 역으로 찾아준다.
 @bp.route('/')
 def index():
-    3/0
+
+    current_app.logger.info("INFO 레벨로 출력")
+
     # url_for 함수에 전달된 'question._list'는 question, _list 순서로 해석되어 함수명을 찾는다.
     # question은 등록된 블루프린트 이름, _list는 블루프린트에 등록된 함수명이다.
     # 현재 _list 함수에 등록된 라우트는 @bp.route('/list/')이므로 
