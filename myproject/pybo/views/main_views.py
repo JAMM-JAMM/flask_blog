9ecbffcb4396e6495ead944d4f17d09cca3eebc7
ShimJaeMin <apvmf32@gmail.com>
2021-07-27 21:00:05 +0900

    chore: error handling

--- myproject/pybo/views/main_views.py
+++ myproject/pybo/views/main_views.py
@@ -13,6 +13,7 @@ def hello_pybo():
 # url_for: 라우트가 설정된 함수명으로 URL을 역으로 찾아준다.
 @bp.route('/')
 def index():
+    3/0
     # url_for 함수에 전달된 'question._list'는 question, _list 순서로 해석되어 함수명을 찾는다.
     # question은 등록된 블루프린트 이름, _list는 블루프린트에 등록된 함수명이다.
     # 현재 _list 함수에 등록된 라우트는 @bp.route('/list/')이므로 
